d954232838170b998685f9ae7d1f91f210c942f4
Elina <129953890+Elinajafari@users.noreply.github.com>
2023-07-30 19:49:34 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5a8f05653e312fd57eede2f9b366c7a878c24e97";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
